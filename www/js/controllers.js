f70b6d3bee6387f7dc3f3ff92d9e1b6563aef750
Jose Manuel Llerena <pepote85@gmail.com>
2015-04-23 10:27:22 +0200

    Incluyo los campos iniciales para la busqueda de gasolinera por localidad y tipo de combustible

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,23 +1,43 @@
 angular.module('reposta.controllers', ['cb.x2js'])
 
-.controller('BuscaCtrl', function($scope, $http, x2js) {
-  $scope.options = [
-    { label: 'one', value: 1 },
-    { label: 'two', value: 2 }
-  ];
+.controller('BuscaCtrl', function($scope, $http, $ionicLoading, $ionicPopup, x2js) {
+
+  $http.get('http://geoportalgasolineras.es/combustibles.do?tipoBusqueda=0').
+    success(function(data, status, headers, config) {
+      $scope.combustibles  = x2js.xml_str2json(data).tiposcombustible.tipocombustible;
+    }).
+    error(function(data, status, headers, config) {
+      $ionicPopup.alert({
+        title: 'Error',
+        template: 'Lo sentimos, ha habido un error al cargar los datos'
+      });
+  });
   
-  $http.get('data/provincias.do').
+  $http.get('http://geoportalgasolineras.es/provincias.do').
     success(function(data, status, headers, config) {
       $scope.provincias  = x2js.xml_str2json(data).provincias.provincia;
     }).
     error(function(data, status, headers, config) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
+      $ionicPopup.alert({
+        title: 'Error',
+        template: 'Lo sentimos, ha habido un error al cargar los datos'
+      });
   });
   
-  $scope.loadCiudades = function() {
-    // Aquí va la lógica para cargar las ciudades de la provincia
-    console.log("Cargar ciudades");  
+  $scope.loadCiudades = function(id_provincia) {
+    var uri = "http://geoportalgasolineras.es/municipios.do?idProvincia=" + id_provincia + "&tipoBusqueda=0";
+    console.log(uri);
+    $http.get(uri).
+      success(function(data, status, headers, config) {
+        $scope.municipios  = x2js.xml_str2json(data).municipios.m;
+        console.log($scope.municipios);
+      }).
+      error(function(data, status, headers, config) {
+        $ionicPopup.alert({
+          title: 'Error',
+          template: 'Lo sentimos, ha habido un error al cargar los datos'
+        });
+    });
   };
 })
 
